6dbfc96f13de569520774c46b212e5a07a274d46
Alejandro Cortes <91970867+acortes2@users.noreply.github.com>
2023-04-09 20:20:30 -0500

    initial commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+/* Efecto escritura */
+const typed = new Typed('.typed', {
+    strings: [
+        'Developer Web', 
+        'DevOps',
+        'Digital Designer',
+        'Digital Advertising'
+    ],
+
+    typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: false, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '_', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+
+});
+
+
+/*Menu*/
+
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
+// Carrousel
